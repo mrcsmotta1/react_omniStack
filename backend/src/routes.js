5df386c131dab26378cdd06a72ccd00a9716c044
Marcos Motta <mrcsmotta1@gmail.com>
2019-08-25 19:38:02 -0300

    Criado Insert de ususarios no mongoDB

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,14 +1,11 @@
 const express = require("express");
-
+const DevController = require('./controllers/DevController');
 const routes = express.Router();
 
 routes.get('/', (req, res) => {
     return res.json({ message: 'Hello Word.' });
 });
 
-routes.post('/devs', (req, res) => {
-    return res.json({ ok: true });
-})
-
+routes.post('/devs', DevController.store);
 
 module.exports = routes;
