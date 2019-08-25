64e6d30514950bec6f518bd6b95bd2567b5dfc7c
Marcos Motta <mrcsmotta1@gmail.com>
2019-08-25 20:44:29 -0300

    Criado listagem de usuarios

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -8,6 +8,8 @@ routes.get('/', (req, res) => {
     return res.json({ message: 'Hello Word.' });
 });
 
+
+routes.get('/devs', DevController.index);
 routes.post('/devs', DevController.store);
 routes.post('/devs/:devId/likes', LikeController.store);
 routes.post('/devs/:devId/dislikes', DislikeController.store);
