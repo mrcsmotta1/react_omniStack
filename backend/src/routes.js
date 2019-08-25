34e916e5f90abf2dfabfa2894194cc08ddc19c44
Marcos Motta <mrcsmotta1@gmail.com>
2019-08-25 20:17:38 -0300

    Criado Like

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const DevController = require('./controllers/DevController');
+const LikeController = require('./controllers/LikeController');
 const routes = express.Router();
 
 routes.get('/', (req, res) => {
@@ -7,5 +8,6 @@ routes.get('/', (req, res) => {
 });
 
 routes.post('/devs', DevController.store);
+routes.post('/devs/:devId/likes', LikeController.store);
 
 module.exports = routes;
