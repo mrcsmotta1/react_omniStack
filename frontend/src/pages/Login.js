f09eb8127e11857e2de9d26bfc9211e73b4a8010
Marcos Motta <mrcsmotta1@gmail.com>
2019-09-10 22:57:28 -0300

    Criado as rotas

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
 import logo from "../assets/logo.svg";
 
-export default function Login() {
+export default function Login({ history }) {
+    const [username, setUsername] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(username);
+        history.push('/main');
+    };
+
     return (
         <div className="login-container">
-            <form>
+            <form onSubmit={handleSubmit}>
                 <img src={logo} alt="Tindev" />
                 <input
                     placeholder="Digite seu usuário do GitHub"
+                    value={username}
+                    onChange={e => setUsername(e.target.value)}
                 />
                 <button type="submit">Enviar</button>
             </form>
