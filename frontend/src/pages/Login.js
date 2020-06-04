daf556fb9cba72ce7765a547f4e52d8b543badd7
Marcos Motta <mrcsmotta1@gmail.com>
2020-06-04 19:01:46 -0300

    Criado o front end e o estilo

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,27 +1,35 @@
-import React, { useState } from 'react';
-import './Login.css';
+import React, { useState } from "react";
+import "./Login.css";
 import logo from "../assets/logo.svg";
 
+import api from "./../services/api";
+
 export default function Login({ history }) {
-    const [username, setUsername] = useState('');
+  const [username, setUsername] = useState("");
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(username);
-        history.push('/main');
-    };
+  async function handleSubmit(e) {
+    e.preventDefault();
 
-    return (
-        <div className="login-container">
-            <form onSubmit={handleSubmit}>
-                <img src={logo} alt="Tindev" />
-                <input
-                    placeholder="Digite seu usuário do GitHub"
-                    value={username}
-                    onChange={e => setUsername(e.target.value)}
-                />
-                <button type="submit">Enviar</button>
-            </form>
-        </div>
-    );
+    const response = await api.post("/devs", {
+      username,
+    });
+
+    const { _id } = response.data;
+
+    history.push(`/dev/${_id}`);
+  }
+
+  return (
+    <div className="login-container">
+      <form onSubmit={handleSubmit}>
+        <img src={logo} alt="Tindev" />
+        <input
+          placeholder="Digite seu usuário do GitHub"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <button type="submit">Enviar</button>
+      </form>
+    </div>
+  );
 }
